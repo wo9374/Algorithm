2c9d4aa43c2b2f2a749467b0a2c7f49cd5a569d5
wo9374 <wo9374@naver.com>
2022-03-07 17:52:39 +0900

    크레인 인형뽑기 진행중

---
+++ app/src/main/java/com/example/algorithm/level1/크레인인형뽑기게임.kt
@@ -0,0 +1,22 @@
+package com.example.algorithm.level1
+
+class 크레인인형뽑기게임 {
+
+    val arr = arrayOf(
+        intArrayOf(0,0,0,0,0),
+        intArrayOf(0,0,1,0,3),
+        intArrayOf(0,2,5,0,1),
+        intArrayOf(4,2,4,4,2),
+        intArrayOf(3,5,1,3,1)
+    )
+
+    val move = intArrayOf(1,5,3,5,1,2,1,4)
+
+    fun solution(board: Array<IntArray>, moves: IntArray): Int {
+        
+
+
+        var answer = 0
+        return answer
+    }
+}
