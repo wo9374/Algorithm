8b0ee281fd5b8cb69d88ce01c42816fa53a870da
wo9374 <wo9374@naver.com>
2022-03-17 18:01:23 +0900

    a

--- app/src/main/java/com/example/algorithm/MainActivity.kt
+++ app/src/main/java/com/example/algorithm/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.example.algorithm.고득점Kit.hash.level2.위장
 import com.example.algorithm.고득점Kit.정렬.level1.K번째_수
 import com.example.algorithm.고득점Kit.정렬.level2.가장큰수
 
@@ -58,13 +59,22 @@ class MainActivity : AppCompatActivity() {
 //            Log.d("가장큰수", 가장큰수().solution(maxInt))
 
 
-            val array = intArrayOf(1,5,2,6,3,7,4)
 
-            var i1 = intArrayOf(2,5,3)
-            var i2 = intArrayOf(4,4,1)
-            var i3 = intArrayOf(1,7,3)
-            val commands = arrayOf(i1,i2,i3)
-            Log.d("K번째 수", "${K번째_수().solution(array, commands)}")
+/*            val array = intArrayOf(1,5,2,6,3,7,4)
+            val commands = arrayOf(
+                intArrayOf(2,5,3),
+                intArrayOf(4,4,1),
+                intArrayOf(1,7,3)
+            )
+            Log.d("K번째 수", "${K번째_수().solution(array, commands)}")*/
+
+
+            val commands = arrayOf(
+                arrayOf("yellow_hat", "headgear"),
+                arrayOf("blue_sunglasses", "eyewear"),
+                arrayOf("green_turban", "headgear"),
+            )
+            Log.d("위장", "${위장().solution(commands)}")
         }
     }
 }
---
+++ app/src/main/java/com/example/algorithm/고득점Kit/hash/level2/위장.kt
@@ -0,0 +1,67 @@
+package com.example.algorithm.고득점Kit.hash.level2
+
+/**
+ * 문제 설명
+스파이들은 매일 다른 옷을 조합하여 입어 자신을 위장합니다.
+
+예를 들어 스파이가 가진 옷이 아래와 같고 오늘 스파이가 동그란 안경, 긴 코트, 파란색 티셔츠를 입었다면 다음날은 청바지를 추가로 입거나 동그란 안경 대신 검정 선글라스를 착용하거나 해야 합니다.
+
+종류	이름
+얼굴	동그란 안경, 검정 선글라스
+상의	파란색 티셔츠
+하의	청바지
+겉옷	긴 코트
+스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+clothes의 각 행은 [의상의 이름, 의상의 종류]로 이루어져 있습니다.
+스파이가 가진 의상의 수는 1개 이상 30개 이하입니다.
+같은 이름을 가진 의상은 존재하지 않습니다.
+clothes의 모든 원소는 문자열로 이루어져 있습니다.
+모든 문자열의 길이는 1 이상 20 이하인 자연수이고 알파벳 소문자 또는 '_' 로만 이루어져 있습니다.
+스파이는 하루에 최소 한 개의 의상은 입습니다.
+
+입출력 예
+                                        clothes	                                            return
+[["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]	5
+[["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]	            3
+
+
+입출력 예 설명
+
+예제 #1
+headgear에 해당하는 의상이 yellow_hat, green_turban이고 eyewear에 해당하는 의상이 blue_sunglasses이므로 아래와 같이 5개의 조합이 가능합니다.
+
+1. yellow_hat
+2. blue_sunglasses
+3. green_turban
+4. yellow_hat + blue_sunglasses
+5. green_turban + blue_sunglasses
+
+
+예제 #2
+face에 해당하는 의상이 crow_mask, blue_sunglasses, smoky_makeup이므로 아래와 같이 3개의 조합이 가능합니다.
+
+1. crow_mask
+2. blue_sunglasses
+3. smoky_makeup
+ * */
+
+class 위장 {
+    fun solution(clothes: Array<Array<String>>): Int {
+        var answer = 0
+
+        // 옷을 입는 방법의 수는 (종류 1 옷의 수) * (종류 2 옷의 수) * ...
+
+        clothes.groupBy {
+            it[1]    //착용부위에 따라 그룹핑 //headgear, eyewear, face 등등
+        }.values.fold(1){
+            acc, list ->  acc * (list.size+1)
+        } - 1        // 스파이는 하루에 최소 한 개의 의상은 입으므로, 아무 것도 안 입는 경우의 수를 하나 제거
+
+        // .fold 메소드는 .reduce와 같이 배열을 순회하며 클로저를 실행해 결과를 acc에 누적하는 역할을 하지만,
+        // .reduce는 배열의 첫 번째 값을 acc의 초깃값으로 사용하는 반면,
+        // .fold를 사용하면 acc의 초깃값을 지정가능
+        return answer
+    }
+}
